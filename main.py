7b245a2a2cf93fb8ff3f546af783785f1ef29e8e
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-12-09 11:17:11 +0530

    Start creating ratio pages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import datetime
+from ratios import ratios_dict
 
 app = Flask(__name__)
 year = datetime.datetime.now().year
@@ -7,6 +8,13 @@ year = datetime.datetime.now().year
 @app.route("/")
 def home():
     return render_template("index.html", year=year)
+
+@app.route("/ratio/<string:ratio>")
+def ratio(ratio):
+    if ratio in ratios_dict:
+        name = ratios_dict[ratio]["name"]
+        description = ratios_dict[ratio]["description"]
+    return render_template("ratio.html", ratio_name=name, ratio_description=description, year=year)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
---
+++ ratios.py
@@ -0,0 +1,12 @@
+# Import this on main.py
+
+ratios_dict = {
+    "gross-margin-percentage":
+        {
+            "name": "Gross Margin Percentage",
+            "short": "Gross margin divided by sales",
+            "description": "Gross margin is sales minus cost of goods sold. If the gross isn't there, there will be no net income."
+                           " How high gross margin percentage needs to be depends on how a business is organized and the costs"
+                           " it has to support. The higher the gross margin percentage, the better."
+         }
+}
